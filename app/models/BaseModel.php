4e61baa935018423facf3f60d0c38d9b97bfa288
Jake Moreman <mail@jakemoreman.co.uk>
2014-06-04 13:47:18 +0100

    Added base model class (forgot to add before)

---
+++ app/models/BaseModel.php
@@ -0,0 +1,18 @@
+<?php
+
+class BaseModel extends Eloquent {
+
+    /**
+     * Disable timestamp management.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Disabled auto-incrementing of id field
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+} 

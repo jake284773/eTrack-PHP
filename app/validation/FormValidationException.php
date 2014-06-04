dbe157c8ee6b9d90ab8bfe50d9f96f7375775237
Jake Moreman <mail@jakemoreman.co.uk>
2014-06-04 13:46:44 +0100

    Added validator classes (forgot to add before)

---
+++ app/validation/FormValidationException.php
@@ -0,0 +1,29 @@
+<?php namespace eTrack\Validation;
+
+use Illuminate\Support\MessageBag;
+
+class FormValidationException extends \Exception {
+
+    /**
+     * @var MessageBag
+     */
+    protected $errors;
+
+    public function __construct($message, MessageBag $errors)
+    {
+        $this->errors = $errors;
+
+        parent::__construct($message);
+    }
+
+    /**
+     * Get form validation errors
+     *
+     * @return MessageBag
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+}

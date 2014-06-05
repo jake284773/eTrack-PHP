f462999fccff48e1a7341e565d32da529735aff4
Jake Moreman <mail@jakemoreman.co.uk>
2014-06-05 11:41:26 +0000

    Added dompdf library to project.

--- app/config/app.php
+++ app/config/app.php
@@ -120,7 +120,8 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
-        'TwigBridge\TwigServiceProvider'
+    'TwigBridge\TwigServiceProvider',
+    'Barryvdh\DomPDF\ServiceProvider',
 
 	),
 
@@ -173,6 +174,7 @@ return array(
 		'Mail'            => 'Illuminate\Support\Facades\Mail',
 		'Paginator'       => 'Illuminate\Support\Facades\Paginator',
 		'Password'        => 'Illuminate\Support\Facades\Password',
+    'PDF'             => 'Barryvdh\DomPDF\Facade',
 		'Queue'           => 'Illuminate\Support\Facades\Queue',
 		'Redirect'        => 'Illuminate\Support\Facades\Redirect',
 		'Redis'           => 'Illuminate\Support\Facades\Redis',
